a0588ee169c955bd03fcef81a8891d4257dc0403
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-04-04 14:14:56 +0200

    exo welcome

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -19,7 +19,14 @@ class MainController extends AbstractController
     // #[Route('/home', name: 'home')]
     // écriture des routes sous Symfony 6.
 
-    // exercice créer une route qui va afficher "bienvenuesur notre site"
+    // exercice créer une route qui va afficher "bienvenue sur notre site"
 
-
+    /**
+     * @Route("/welcome", name="welcome")
+     */
+    public function welcome()
+    {
+        var_dump("bienvenue sur notre site");
+        die;
+    }
 }
