7ec877406fe6fe8a85fbccf251e0700747b140b3
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-04-06 17:00:32 +0200

    show post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -21,12 +21,23 @@ class PostController extends AbstractController
 
     /**
      * @Route("posts", name="post_list")
-     */                         // autowire ou autowiring
+     */                         // autowire ou autowirering
     public function postList(PostRepository $postRepository)
     {
         // la méthode findAll récupère tous les posts de la base de données.
         $posts = $postRepository->findAll();
 
         return $this->render("post_list.html.twig", ['posts' => $posts]);
+    }
+
+    /**
+     * @Route("post/{id}", name="post_show")
+     */
+    public function postShow($id, PostRepository $postRepository)
+    {
+        // la méthode find permet de récuperer un post en focntion de son id
+        $post = $postRepository->find($id);
+
+        return $this->render("post_show.html.twig", ['post' => $post]);
     }
 }
