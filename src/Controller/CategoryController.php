97d2a5b4816fa2292f0106317c48b2ce36d160f5
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-04-07 12:19:32 +0200

    update category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -41,4 +42,28 @@ class CategoryController extends AbstractController
 
     // Exercice créer les deux méthodes qui permettent d'afficher la liste des tag
     // et un tag en particulier à l'aide de son id
+
+    /**
+     * @Route("update/category/{id}", name="category_update")
+     */
+    public function categoryUpdate(
+        $id,
+        CategoryRepository $categoryRepository,
+        EntityManagerInterface $entityManagerInterface
+    ) {
+        $category = $categoryRepository->find($id);
+
+        $category->setName("Nouvelle catégorie modifiée");
+
+        // la fonction persist va regarder ce que l'on a fait sur category et
+        // réaliser le code pour faire le CREATE ou le UPDATE en fonction de l'origine de la category  
+        $entityManagerInterface->persist($category);
+        // la fonction flush enregistre dans la bdd.
+        $entityManagerInterface->flush();
+
+        return $this->redirectToRoute('category_list');
+    }
+
+    // exerice créer une méthode qui modifie le name d'un tag en fonction de son id et le name sera :
+    // nouveau tag modifié
 }
