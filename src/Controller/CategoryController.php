bae2cf8cdf6e12d34d0391e33f89644414bc77d1
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-04-07 11:07:34 +0200

    read category

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/cateory", name="app_cateory")
+     */
+    public function index(): Response
+    {
+        return $this->render('cateory/index.html.twig', [
+            'controller_name' => 'CateoryController',
+        ]);
+    }
+
+    /**
+     * @Route("categories", name="category_list")
+     */                             // autowire
+    public function categoryList(CategoryRepository $categoryRepository)
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render("categories_list.html.twig", ['categories' => $categories]);
+    }
+
+    /**
+     * @Route("category/{id}", name="category_show")
+     */
+    public function categoryShow(CategoryRepository $categoryRepository, $id)
+    {
+        $category = $categoryRepository->find($id);
+
+        return $this->render("category_show.html.twig", ['category' => $category]);
+    }
+}
